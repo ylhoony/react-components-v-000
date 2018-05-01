099a9e5fff92adf4c59f1acfa85cde0f2b5af225
Hoon Lee <yl.hoony@gmail.com>
2018-05-01 01:15:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 class Comment extends React.Component {
   render() {
     return (
-      <div>
-        i am a hard coded comment! help! make me dynamic!
+      <div className="comment">
+        {this.props.content}
+      </div>
+    )
+  }
+}
+
+class BlogPost extends React.Component {
+  render() {
+    return (
+      <div id="blog-post">
+
+        <div id="blog-content">
+          Dear Reader: Bjarne Stroustrup has the perfect lecture oration.
+        </div>
+
+        <Comment content={"I agree with this statement. - Angela Merkel"}/>
+        <Comment content={"A universal truth. - Noam Chomsky"}/>
+        <Comment content={"Truth is singular. Its ‘versions’ are mistruths. - Sonmi-451"}/>
+
       </div>
     )
   }
